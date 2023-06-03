bd070847d848e5fdf513463947842c20a68485c3
Jordan Olguin <jordanolguin@iMac.local>
2023-06-03 15:47:02 -0600

    initial queryURL

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,9 @@
-console.log("hello world");
+var apiKey = "5800608481ba95f3f6584cd0785fd228";
+var city;
+var queryURL =
+  "http://api.openweathermap.org/data/2.5/weather?q=" +
+  city +
+  "&appid=" +
+  apiKey;
+
+fetch(queryURL);
