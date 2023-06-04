4b36539594f799e01166e171844f15e16faa5d5d
Jordan Olguin <jordanolguin@iMac.local>
2023-06-03 22:06:05 -0600

    start over JS

--- assets/script.js
+++ assets/script.js
@@ -1,51 +1,20 @@
-$(function () {
-  var today = dayjs();
-  $("#currentDay").text(today.format("dddd, MMM D"));
-});
-
 var userFormEl = document.querySelector("#user-form");
-var cityInputEl = document.querySelector("#cityName");
-var currentCityContainerEl = document.querySelector("#weather-container");
+var cityInput = document.querySelector("#cityName");
+var weatherContainerEl = document.querySelector("#weather-container");
 
 var formSubmitHandler = function (event) {
   event.preventDefault();
 
-  var cityName = cityInputEl.value.trim();
+  var city = cityInput.value.trim();
 
-  if (cityName) {
-    getCityInfo(cityName);
+  if (city) {
+    getWeatherInfo(city);
 
-    currentCityContainerEl.textContent = "";
-    cityInputEl.value = "";
+    weatherContainerEl.textContent = "";
+    cityInput.value = "";
   } else {
-    alert("Please enter City name");
+    alert("Please enter valid city name");
   }
-};
-
-var getCityInfo = function (city) {
-  var apiKey = "5800608481ba95f3f6584cd0785fd228";
-  var queryURL =
-    "https://api.openweathermap.org/data/2.5/weather?q=" +
-    city +
-    "&appid=" +
-    apiKey;
-
-  fetch(queryURL)
-    .then(function (response) {
-      if (response.ok) {
-        return response.json();
-      } else {
-        throw new Error("Error: " + response.statusText);
-      }
-    })
-    .then(function (data) {
-      console.log(data);
-      displayWeather();
-    })
-    .catch(function (error) {
-      console.log(error);
-      alert("Unable to connect to Weather Data");
-    });
 };
 
 userFormEl.addEventListener("submit", formSubmitHandler);
