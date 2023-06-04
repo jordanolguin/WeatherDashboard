c9b04224bd358178e079b2213a91418b659d94e4
Jordan Olguin <jordanolguin@iMac.local>
2023-06-03 21:35:11 -0600

    api works with submit button

--- assets/script.js
+++ assets/script.js
@@ -1,63 +1,51 @@
-// var userFormEl = document.querySelector("#user-form");
-// var cityInputEl = document.querySelector("#cityName");
-// var currentCityContainerEl = document.querySelector("#weather-container");
+$(function () {
+  var today = dayjs();
+  $("#currentDay").text(today.format("dddd, MMM D"));
+});
 
-// var formSubmitHandler = function (event) {
-//   event.preventDefault();
+var userFormEl = document.querySelector("#user-form");
+var cityInputEl = document.querySelector("#cityName");
+var currentCityContainerEl = document.querySelector("#weather-container");
 
-//   var cityName = cityInputEl.value.trim();
+var formSubmitHandler = function (event) {
+  event.preventDefault();
 
-//   if (cityName) {
-//     getCityInfo(cityName);
+  var cityName = cityInputEl.value.trim();
 
-//     currentCityContainerEl.textContent = "";
-//     cityInputEl.value = "";
-//   } else {
-//     alert("Please enter City name");
-//   }
-// };
+  if (cityName) {
+    getCityInfo(cityName);
 
-// var getCityInfo = function (city) {
-//   var apiKey = "5800608481ba95f3f6584cd0785fd228";
-//   var city = cityInputEl.value.trim();
-//   var queryURL =
-//     "http://api.openweathermap.org/data/2.5/weather?q=" +
-//     city +
-//     "&appid=" +
-//     apiKey;
+    currentCityContainerEl.textContent = "";
+    cityInputEl.value = "";
+  } else {
+    alert("Please enter City name");
+  }
+};
 
-//   fetch(queryURL)
-//     .then(function (response) {
-//       if (response.ok) {
-//         response.json().then(function (data) {
-//           displayWeather();
-//         });
-//       } else {
-//         alert("Error: " + response.statusText);
-//       }
-//     })
-//     .catch(function (error) {
-//       alert("Unable to connect to Weather Data");
-//     });
-// };
+var getCityInfo = function (city) {
+  var apiKey = "5800608481ba95f3f6584cd0785fd228";
+  var queryURL =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    city +
+    "&appid=" +
+    apiKey;
 
-// var displayWeather = function ()
+  fetch(queryURL)
+    .then(function (response) {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("Error: " + response.statusText);
+      }
+    })
+    .then(function (data) {
+      console.log(data);
+      displayWeather();
+    })
+    .catch(function (error) {
+      console.log(error);
+      alert("Unable to connect to Weather Data");
+    });
+};
 
-// userFormEl.addEventListener("submit", formSubmitHandler);
-
-// var apiKey = "5800608481ba95f3f6584cd0785fd228";
-// var city;
-// var queryURL =
-//   "http://api.openweathermap.org/data/2.5/weather?q=" +
-//   city +
-//   "&appid=" +
-//   apiKey;
-fetch(
-  "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=5800608481ba95f3f6584cd0785fd228"
-)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-  });
+userFormEl.addEventListener("submit", formSubmitHandler);
