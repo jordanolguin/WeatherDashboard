961049bdf40ee713e9f2d95e8574519bc03583fc
Jordan Olguin <jordanolguin@iMac.local>
2023-06-06 20:43:05 -0600

    fiveDayForcast function

--- assets/script.js
+++ assets/script.js
@@ -18,6 +18,43 @@ var formSubmitHandler = function (event) {
   }
 };
 
+//capturing and appending current weather to DOM
+var displayCurrentWeather = function (dataObj) {
+  console.log(dataObj);
+  var cityEl = document.createElement("h1");
+  var tempEl = document.createElement("p");
+  var windEl = document.createElement("p");
+  var humidityEl = document.createElement("p");
+  cityEl.textContent = dataObj.name;
+  tempEl.textContent = dataObj.main.temp;
+  windEl.textContent = dataObj.wind.speed;
+  humidityEl.textContent = dataObj.main.humidity;
+  weatherContainerEl.append(cityEl, tempEl, windEl, humidityEl);
+};
+
+//capturing and appending five day forcast
+var displayFiveDayForcast = function (array) {
+  var filteredArray = array.filter(
+    (obj) => obj.dt_txt.split(" ")[1] === "12:00:00"
+  );
+  console.log(filteredArray);
+  for (let i = 0; i < filteredArray.length; i++) {
+    var forecastItem = filteredArray[i];
+    var forecastContainer = document.createElement("div");
+    var dateEl = document.createElement("h2");
+    var tempEl = document.createElement("p");
+    var windEl = document.createElement("p");
+    var humidityEl = document.createElement("p");
+    var date = forecastItem.dt_txt.split(" ")[0];
+    dateEl.textContent = date;
+    tempEl.textContent = "Temperature: " + forecastItem.main.temp;
+    windEl.textContent = "Wind Speed: " + forecastItem.wind.speed;
+    humidityEl.textContent = "Humidity: " + forecastItem.main.humidity;
+    forecastContainer.append(dateEl, tempEl, windEl, humidityEl);
+    weatherContainerEl.appendChild(forecastContainer);
+  }
+};
+
 // capture City input coordinates for weather function
 var getCoordinates = function (coordinates) {
   var cityName = cityInput.value.trim();
@@ -28,8 +65,7 @@ var getCoordinates = function (coordinates) {
     // "http://api.openweathermap.org/geo/1.0/direct?q=" +
     "https://api.openweathermap.org/data/2.5/weather?q=" +
     cityName +
-    // "&limit=" +
-    // limit +
+    "&units=imperial" +
     "&appid=" +
     apiKey;
 
@@ -50,6 +86,7 @@ var getCoordinates = function (coordinates) {
             ", Longitude = " +
             lon
         );
+        displayCurrentWeather(data);
         getForcast(lat, lon);
       } else {
         console.log("No coordinates found for " + cityName + ".");
@@ -63,21 +100,23 @@ var getCoordinates = function (coordinates) {
 // plug lat and lon values into api
 var getForcast = function (latitude, longitude) {
   var apiKey = "5800608481ba95f3f6584cd0785fd228";
-
-  var weatherUrl =
+  // var forecastURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}`
+  var forcastUrl =
     "https://api.openweathermap.org/data/2.5/forecast?lat=" +
     latitude +
     "&lon=" +
     longitude +
+    "&units=imperial" +
     "&appid=" +
     apiKey;
 
-  fetch(weatherUrl)
+  fetch(forcastUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      console.log("Weather Data: ", data);
+      // console.log("Weather Data: ", data);
+      displayFiveDayForcast(data.list);
     })
     .catch(function (error) {
       console.log(error);
