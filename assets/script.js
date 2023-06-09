becc453cc2fcd3d8a49180b6932e723388b02f41
Jordan Olguin <jordanolguin@iMac.local>
2023-06-09 17:29:48 -0600

    final date and icons added

--- assets/script.js
+++ assets/script.js
@@ -71,22 +71,37 @@ var saveSearch = function (city) {
 var displayCurrentWeather = function (dataObj) {
   weatherContainerEl.textContent = "";
   console.log(dataObj);
+
   var cityEl = document.createElement("h2");
+  var dateEl = document.createElement("span");
   var iconEl = document.createElement("img");
   var tempEl = document.createElement("p");
   var windEl = document.createElement("p");
   var humidityEl = document.createElement("p");
+
   cityEl.textContent = dataObj.name;
+  dateEl.textContent = " - " + getCurrentDate();
+
   var iconCode = dataObj.weather[0].icon;
   var iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
   iconEl.src = iconUrl;
-  iconEl.style.width = "50px";
-  iconEl.style.height = "50px";
+  iconEl.style.width = "55px";
+  iconEl.style.height = "55px";
+
   tempEl.textContent = "Temperature: " + dataObj.main.temp + "°F";
   windEl.textContent = "Wind Speed: " + dataObj.wind.speed + " mph";
   humidityEl.textContent = "Humidity: " + dataObj.main.humidity + "%";
+
+  cityEl.append(dateEl);
   weatherContainerEl.append(cityEl, iconEl, tempEl, windEl, humidityEl);
 };
+
+//set current date in the format "Month Day, Year"
+function getCurrentDate() {
+  var currentDate = new Date();
+  var options = { year: "numeric", month: "long", day: "numeric" };
+  return currentDate.toLocaleDateString(undefined, options);
+}
 
 //capturing and appending five day forcast
 var displayFiveDayForcast = function (array) {
@@ -99,21 +114,32 @@ var displayFiveDayForcast = function (array) {
     var forecastItem = filteredArray[i];
     var forecastContainer = document.createElement("div");
     var dateEl = document.createElement("h2");
+    var forecastIconEl = document.createElement("img");
     var tempEl = document.createElement("p");
     var windEl = document.createElement("p");
     var humidityEl = document.createElement("p");
-    var date = forecastItem.dt_txt.split(" ")[0];
+    var fullDate = forecastItem.dt_txt.split(" ")[0];
+    var dateComponents = fullDate.split("-");
+    var date = dateComponents[1] + "-" + dateComponents[2];
+
     forecastContainer.classList.add("individual-forecast-card");
     forecastContainer.classList.add("col-12");
     forecastContainer.classList.add("col-md-2");
     dateEl.textContent = date;
-    // var iconCode = dataObj.weather[0].icon;
-    // var iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
-    // iconEl.src = iconUrl;
+    var forecastIconCode = forecastItem.weather[0].icon;
+    var forecastIconUrl =
+      "http://openweathermap.org/img/w/" + forecastIconCode + ".png";
+    forecastIconEl.src = forecastIconUrl;
     tempEl.textContent = "Temperature: " + forecastItem.main.temp + "°F";
     windEl.textContent = "Wind Speed: " + forecastItem.wind.speed + " mph";
     humidityEl.textContent = "Humidity: " + forecastItem.main.humidity + "%";
-    forecastContainer.append(dateEl, tempEl, windEl, humidityEl);
+    forecastContainer.append(
+      dateEl,
+      forecastIconEl,
+      tempEl,
+      windEl,
+      humidityEl
+    );
     fiveDayForcastEl.appendChild(forecastContainer);
   }
 };
